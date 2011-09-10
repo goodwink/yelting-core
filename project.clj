37359251cdfdf10be4d095ef165e88946c235aeb
Kyle S. Goodwin <kyle@devscoop.com>
2011-09-10 18:13:37 -0400

    Host now working and verified with fleet-db.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject yelting-core "1.0-SNAPSHOT"
   :description "TODO: add summary of your project"
-  :dependencies [[clojure "1.2.0"]
+  :dependencies [[clojure "1.2.1"]
 		 [clj-time "0.3.0"]
 		 [com.mefesto/wabbitmq "0.1.4"]
 		 [org.clojure/data.json "0.1.1"]
-		 [goodwink/fleetdb-client "0.2.2"]
+		 [goodwink/fleetdb-client "0.2.3"]
 		 [yelting-achlib "1.0-SNAPSHOT"]])
--- src/yelting_core/host.clj
+++ src/yelting_core/host.clj
@@ -2,9 +2,12 @@
   (:import (java.io FileWriter FileReader PushbackReader))
   (:use [yelting-achlib.parser]
         [clj-time.core :exclude 'extend]
+	[clj-time.coerce :only (to-string from-string)]
 	[fleetdb.client]))
 
 (def client (connect))
+
+(defn guid [] (.toString (java.util.UUID/randomUUID)))
 
 (defn- for-account [account-number]
   {:where ["=" :account-number account-number]})
@@ -13,19 +16,22 @@
   {:where ["=" :customer-id customer-id]})
 
 (defn- add-memo [account-number amount description]
-  [:insert :memos {:account-number account-number :amount amount :description description :sent-at (now)}])
+  [:insert :memos {:id (guid) :account-number account-number :amount amount :description description :sent-at (to-string (now))}])
 
 (defn- account-and-memos [account-number]
-  (client ([:multi-read
-	    [:select :accounts (for-account account-number)]
-	    [:select :memos (for-account account-number)]])))
+  (client [:multi-read
+	   [[:select :accounts (for-account account-number)]
+	    [:select :memos (for-account account-number)]]]))
 
 (defn available-balance
   ([account memos]
-     (reduce + (:ledger-balance account) memos))
+     (reduce + (:ledger-balance account) (map :amount memos)))
   ([account-number]
      (let [[[account] memos] (account-and-memos account-number)]
        (reduce + (:ledger-balance account) memos))))
+
+(defn current-memos [account-number]
+  (client [:select :memos (for-account account-number)]))
 
 (defn post-memos
   ([]
@@ -33,27 +39,39 @@
        (cond
 	(empty? accounts) true
 	:else
-	(let [result (post-memos (first accounts) (client [:select :memos (for-account (:account-number (first accounts)))]))]
+	(let [result (post-memos
+		      (first accounts)
+		      (current-memos (:account-number (first accounts))))]
 	  (cond
 	   (first result) (recur (rest accounts))
-	   :else (recur (conj (rest accounts) (first (client [:select :accounts (for-account (:account-number (first accounts)))])))))))))
-  ([[account memos]]
+	   :else (recur
+		  (conj
+		   (rest accounts)
+		   (first
+		    (client [:select :accounts
+			     (for-account (:account-number (first accounts)))])))))))))
+  ([account memos]
      (client
       [:checked-write
-       [:select :accounts (for-account (:account-number account))]
-       (:ledger-balance account)
+       [:select :accounts {:where ["=" :account-number (:account-number account)]
+			   :only :ledger-balance}]
+       [(:ledger-balance account)]
        [:multi-write
-	[:update :accounts {:ledger-balance (available-balance account memos)}]
-	[:insert :posted-transactions (map (fn[memo] (assoc memo :posted-at (now))) memos)]
-	[:delete :memos (for-account (:account-number account))]]])))
+	[[:update :accounts
+	  {:ledger-balance (available-balance account memos)}
+	  (for-account (:account-number account))]
+	 [:insert :posted-transactions
+	  (map (fn[memo] (assoc memo :posted-at (to-string (now)) :id (guid))) memos)]
+	 [:delete :memos
+	  (for-account (:account-number account))]]]])))
 
 (defn post-ach [records]
   )
 
 (defn transfer [from-account-number to-account-number amount description]
   (client [:multi-write 
-	   (add-memo from-account-number (- amount) description)
-	   (add-memo to-account-number amount description)]))
+	   [(add-memo from-account-number (- amount) description)
+	    (add-memo to-account-number amount description)]]))
 
 (defn has-memo? [account-number]
   (> 0 (client [:count :memos (for-account account-number)])))
@@ -63,10 +81,7 @@
    (first (client [:select :accounts (for-account account-number)]))))
 
 (defn transaction-history [account-number]
-  [:select :posted-transactions (for-account account-number)])
-
-(defn current-memos [account-number]
-  [:select :memos (for-account account-number)])
+  (client [:select :posted-transactions (for-account account-number)]))
 
 (defn account-details [account-number]
   (let [[[account] memos] (account-and-memos account-number)]
@@ -75,6 +90,18 @@
 	    :available-balance (available-balance account memos)})))
 
 (defn customer-details [customer-id]
-  (first (client [:select :customers (for-customer customer-id)])))
+  (let [customer (first (client [:select :customers (for-customer customer-id)]))
+	accounts (client [:select :customer-accounts (for-customer customer-id)])]
+    (assoc customer :accounts accounts)))
 
-(defn create-account [])
+(defn create-customer [customer-id first-name last-name legal-name tax-id is-business?]
+  (client [:insert :customers
+	   {:customer-id customer-id :first-name first-name :last-name last-name
+	    :legal-name legal-name :tax-id tax-id :is-business? is-business? :id (guid)}]))
+
+(defn create-account [customer-id account-number account-type]
+  (client [:multi-write
+	   [[:insert :accounts
+	     {:account-number account-number :account-type account-type :ledger-balance 0 :id (guid)}]
+	    [:insert :customer-accounts
+	     {:customer-id customer-id :account-number account-number :id (guid)}]]]))
